43964638245395d5bccefd6988b38cbbba334ec2
Dennis Streit <mail.dstreit@gmail.com>
2018-06-06 11:00:52 +0200

    basics

--- src/main/java/com/bordercloud/sparql/Endpoint.java
+++ src/main/java/com/bordercloud/sparql/Endpoint.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package com.dennis.streit;
+package com.bordercloud.sparql;
 
 import java.io.IOException;
 import java.io.StringReader;
--- src/main/java/com/dennis/streit/Main.java
+++ src/main/java/com/dennis/streit/Main.java
@@ -1,53 +1,89 @@
 package com.dennis.streit;
 
+import com.bordercloud.sparql.*;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import org.json.*;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Main {
-    public static void main(String[] args) {
+
+public class Main
+{
+
+
+    public static void main(String[] args){
         try {
-            String querySelect =
-                    "SELECT ?item ?itemLabel \n" +
-                    "WHERE \n" +
-                    "{\n" +
-                    "  ?item wdt:P31 wd:Q146.\n" +
-                    "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],en\". }\n" +
-                    "}";
+        String WDEndpoint = "https://query.wikidata.org/sparql";
+        String queryCities =
+            "SELECT DISTINCT ?cityLabel ?population ?countryLabel ?cityDescription WHERE {\n" +
+                    "	?city wdt:P31/wdt:P279* ?settlement\n" +
+                    "    FILTER(?settlement = wd:Q515 || ?settlement = wd:Q15284)      \n" +
+                    "	?city wdt:P1082 ?population .\n" +
+                    "	?city wdt:P17 ?country .\n" +
+                    "\n" +
+                    "  FILTER(?population > 25000) \n" +
+                    "  SERVICE wikibase:label {\n" +
+                    "		bd:serviceParam wikibase:language \"en\" .\n" +
+                    "  }\n" +
+                    "}\n" +
+                    "LIMIT 10";
 
 
-            String endpoint = "https://query.wikidata.org/sparql";
-            System.out.println("");
-            System.out.println("Endpoint : " + endpoint);
-            System.out.println("");
-            System.out.println("Result : ");
+            ResultsToJson(getQueryResults(WDEndpoint, queryCities));
+            printResults(getQueryResults(WDEndpoint, queryCities), 40);
 
-            Endpoint sp2 = new Endpoint(endpoint, false);
-            HashMap<String, HashMap> rs2 = sp2.query(querySelect);
-            printResult(rs2, 30);
 
         } catch (EndpointException eex) {
             System.out.println(eex);
             eex.printStackTrace();
         }
+
+
     }
 
-    public static void printResult(HashMap<String, HashMap> rs, int size) {
+    public static HashMap<String, HashMap>  getQueryResults (String WDendpoint, String WDquery) throws EndpointException{
+        Endpoint endpoint = new Endpoint(WDendpoint, false);
+        endpoint.setMethodHTTPRead("GET");
+        HashMap<String, HashMap> queryResults = endpoint.query(WDquery);
+        return queryResults;
+    }
 
-        for (String variable : (ArrayList<String>) rs.get("result").get("variables")) {
-            System.out.print(String.format("%-" + size + "." + size + "s", variable) + " | ");
-        }
-        System.out.print("\n");
-        for (HashMap<String, Object> value : (ArrayList<HashMap<String, Object>>) rs.get("result").get("rows")) {
-            //System.out.print(value);
-            /* for (String key : value.keySet()) {
-         System.out.println(value.get(key));
-         }*/
-            for (String variable : (ArrayList<String>) rs.get("result").get("variables")) {
-                //System.out.println(value.get(variable));
+
+    public static void printResults(HashMap<String, HashMap> queryResults, int size)
+    {
+        for (HashMap<String, Object> value : (ArrayList<HashMap<String, Object>>) queryResults.get("result").get("rows")) {
+            for (String variable : (ArrayList<String>) queryResults.get("result").get("variables")) {
                 System.out.print(String.format("%-" + size + "." + size + "s", value.get(variable)) + " | ");
             }
             System.out.print("\n");
         }
     }
 
+
+    public static void ResultsToJson(HashMap<String, HashMap> queryResults)
+    {
+        JSONArray jArray = new JSONArray();
+
+        for (HashMap<String, Object> value : (ArrayList<HashMap<String, Object>>) queryResults.get("result").get("rows")) {
+            JSONObject jObj = new JSONObject();
+            for (String variable : (ArrayList<String>) queryResults.get("result").get("variables")) {
+                jObj.put(variable, value.get(variable));
+            }
+            jArray.put(jObj);
+        }
+
+        System.out.println(jArray.toString());
+        try {
+            FileWriter file = new FileWriter("files/results.json");
+            file.write(jArray.toString());
+            file.flush();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 }
